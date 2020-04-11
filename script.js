6ae5ed687557d55175e3b71ab5e4be235df1e313
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-04-11 05:06:26 +0200

    corrected

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const joke = document.getElementById('joke');
-const getJoke = document.getElementById('getJoke');
-
-getJoke.addEventListener('click', generateJoke);
 
 generateJoke();
 
 
 function generateJoke() {
 // API
-var request = new XMLHttpRequest()
+const request = new XMLHttpRequest()
 
 request.open('GET', 'https://bridge.buddyweb.fr/api/blagues/blagues', true)
 request.onload = function() {
 
   //DB
-  var data = JSON.parse(this.response)
+  const data = JSON.parse(this.response)
 
   // PICK A RANDOM JOKE
-  var item = data[Math.floor(Math.random() * data.length)]
+  const item = data[Math.floor(Math.random() * data.length)]
 
   joke.innerHTML = `${item.blagues}`;
   
