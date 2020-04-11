017047067a174781768473aa19b8912594c9dcc6
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-04-11 13:29:42 +0200

    corrected 2.0

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
-const joke = document.getElementById('joke');
-
 generateJoke();
 
-
 function generateJoke() {
-// API
-const request = new XMLHttpRequest()
+  // API
+  const request = new XMLHttpRequest()
 
-request.open('GET', 'https://bridge.buddyweb.fr/api/blagues/blagues', true)
-request.onload = function() {
-
-  //DB
+  request.open('GET', 'https://bridge.buddyweb.fr/api/blagues/blagues', true)
+  request.onload = function() {
+  // DB
   const data = JSON.parse(this.response)
 
   // PICK A RANDOM JOKE
   const item = data[Math.floor(Math.random() * data.length)]
 
-  joke.innerHTML = `${item.blagues}`;
-  
-}
-request.send()
+  document.getElementById('joke').innerHTML = `${item.blagues}`;
+  };
+
+  request.send()
 };
